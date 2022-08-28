96db2e12b8bee03913c162a6d2edd90b4fa8a7d3
Alberto Chacón <josealberto.ch.b@gmail.com>
2022-08-28 16:52:59 -0600

    86. React Router Link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import Navigation from './routes/navigation/navigation.component';
 
 import Home from "./routes/home/home.component";
-
-const Navigation = () => {
-  return(
-    <div>
-      <div>
-        <h1>I am the Navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
 
 const App = () => {
   return (
