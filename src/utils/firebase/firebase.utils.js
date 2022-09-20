bcc527c1625849afa893dc7d02178e3a22e4d334
Alberto Chacón <josealberto.ch.b@gmail.com>
2022-09-19 21:12:41 -0600

    94. Setting Up User Documents

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,6 +10,13 @@ import {
   GoogleAuthProvider
 } from 'firebase/auth';
 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore'
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDdoku69WfnNQxd5N-NHPNIkt6UTOR4LQ4",
@@ -32,3 +39,15 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists())
+}
