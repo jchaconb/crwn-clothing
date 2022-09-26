a9bbcd34fbebb56e4d8c79e6c393fface5abc9e8
Alberto Chacón <josealberto.ch.b@gmail.com>
2022-09-25 19:33:37 -0600

    96. Sign In With Redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth 
 } from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
+  useEffect( () => {
+    async function fetchData(){
+      const response = await getRedirectResult(auth);
+      if(response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+    }
+    fetchData();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef =  await createUserDocumentFromAuth(user);
@@ -13,6 +28,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
     </div>
   )
 }
