9378e8db72de239ff6fa50576ca6a192c2f5784f
Alberto Chacón <josealberto.ch.b@gmail.com>
2023-01-31 21:15:54 -0600

    sets up firestore and user creation

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -10,6 +10,13 @@ import {
   GoogleAuthProvider
 } from 'firebase/auth';
 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore';
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD2KkN6L8WCX8SjXTnvV9Q63yS1n6zNCUw",
@@ -31,3 +38,13 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+};
