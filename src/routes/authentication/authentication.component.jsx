e520b483b52ac37017adad374771e2c506b2929f
Alberto Chacón <josealberto.ch.b@gmail.com>
2023-02-07 17:17:39 -0600

    renames component SignIn to Authentication

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -4,7 +4,7 @@ import {
   createUserDocumentFromAuth
 } from "../../utils/firebase/firebase.utils";
 
-const SignIn = () => {
+const Authentication = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     await createUserDocumentFromAuth(user);
@@ -19,4 +19,4 @@ const SignIn = () => {
   )
 }
 
-export default SignIn;
+export default Authentication;
