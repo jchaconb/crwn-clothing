8acb117b45715cc8c1ba55db5090b023e0dd4289
Alberto Chacón <josealberto.ch.b@gmail.com>
2023-02-18 17:30:44 -0600

    adds cart count logic

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,14 +1,19 @@
-import './cart-item.styles.scss';
- 
-const CartItem = ({ cartItem }) => {
-  const { name, quantity } = cartItem;
+import "./cart-item.styles.scss";
 
-  return(
-    <div>
-      <h2>{name}</h2>
-      <span>{quantity}</span>
+const CartItem = ({ cartItem }) => {
+  const { name, quantity, imageUrl, price } = cartItem;
+
+  return (
+    <div className="cart-item-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default CartItem;
