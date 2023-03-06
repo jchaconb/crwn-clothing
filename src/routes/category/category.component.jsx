94d8853f02137a8898df4718b6a2df6be31177b7
Alberto Chacón <josealberto.ch.b@gmail.com>
2023-03-05 19:19:11 -0600

    fixes clashing styles

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
 
 import ProductCard from "../../components/product-card/product-card.component";
@@ -12,25 +12,20 @@ const Category = () => {
   const { categoriesMap } = useContext(CategoriesContext);
   const [products, setProducts] = useState(categoriesMap[category]);
 
-  console.log(category);
-  console.log(products);
-  console.log(categoriesMap);
-
   useEffect(() => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
 
-  console.log(category);
-  console.log(products);
-  console.log(categoriesMap);
-
   return (
-    <div className="category-container">
-      {products &&
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
+    <Fragment>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </Fragment>
   );
 };
 
