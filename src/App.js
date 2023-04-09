1a816ca04eed9e4875d0ad28fa66ec5e0c3c0c64
Alberto Chacón <josealberto.ch.b@gmail.com>
2023-04-08 19:13:55 -0600

    adds configuration / sets up the Store's application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import { Routes, Route } from 'react-router-dom';
 
-import Navigation from "./routes/navigation/navigation.component";
-import Home from "./routes/home/home.component";
-import Authentication from './routes/authentication/authentication.component';
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from "./utils/firebase/firebase.utils";
 
+import Home from "./routes/home/home.component";
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from './routes/authentication/authentication.component';
+
+import { setCurrentUser } from "./store/user/user.actions";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, []);
+
+
   return(
     <Routes>
       <Route path='/' element={<Navigation />}>
