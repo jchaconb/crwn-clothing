08163492f11d8bf406fd69f60262c72fdd134b9c
Alberto Chacón <josealberto.ch.b@gmail.com>
2023-04-28 19:38:35 -0600

    [159] transforms category selector to use business logic

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,7 +5,7 @@ import { useDispatch } from "react-redux";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategoriesMap } from "../../store/categories/category.action";
+import { setCategories } from "../../store/categories/category.action";
 
 import "./shop.styles.scss";
 
@@ -15,7 +15,7 @@ const Shop = () => {
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoriesArray = await getCategoriesAndDocuments("categories");
-      dispatch(setCategoriesMap(categoriesArray));
+      dispatch(setCategories(categoriesArray));
     };
 
     getCategoriesMap();
