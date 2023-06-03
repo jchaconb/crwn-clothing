5698c9973ce8531d101b2d80a7e406f9d5def1ff
Alberto Chacón <josealberto.ch.b@gmail.com>
2023-06-02 19:46:23 -0600

    Redux-Saga: fetchCategoriesAsync Thunk to Saga

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,17 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import { rootReducer } from './root-reducer';
+import { rootSaga } from './root-saga';
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger, thunk].filter(
-  Boolean
-);
+const sagaMiddleware = createSagaMiddleware();
+
+const middleWares = [
+  process.env.NODE_ENV === 'development' && logger,
+  sagaMiddleware,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
@@ -31,5 +35,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
